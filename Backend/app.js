d912588e3eeffcf14266c08cc1a03b8348f06f1e
Piyush-bansaI <kajal891bansal@gmail.com>
2026-01-31 15:40:38 +0530

    ANIbro - some changes

--- Backend/app.js
+++ Backend/app.js
@@ -16,7 +16,7 @@ app.use(cookie())
 app.use(express.json())
 app.use(morgan("dev"))
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: 'https://anibro-lzcx9hdah-piyush-bansais-projects.vercel.app',
     credentials: true
 }))
 const recommendRouter = require('./routes/AI.routes');
