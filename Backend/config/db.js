80af49c35a7bcd4534530515aad8d3fe116e11b8
Piyush-bansaI <kajal891bansal@gmail.com>
2026-01-31 18:02:06 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose")
 const connectTodb = () => {
     mongoose.connect(process.env.ANIbro_URI).then(() => {
         console.log("\x1b[1;92mConnected to database\x1b[0m")
+    }).catch((e) => {
+        console.log("\x1b[1;91mSonething went wrong\x1b[0m", e)
     })
 }
 
