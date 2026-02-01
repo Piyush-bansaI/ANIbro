9348788f1e98579b28ede5b227314e16a3e10952
Piyush-bansaI <kajal891bansal@gmail.com>
2026-02-01 18:44:43 +0530

    Update ProfilePicContext.jsx

--- Frontend/ANIbro_frontend/src/Contexts/ProfilePicContext.jsx
+++ Frontend/ANIbro_frontend/src/Contexts/ProfilePicContext.jsx
@@ -26,10 +26,8 @@ const ProfilePicContext = ({children}) => {
       if (!brosData) return
 
       const getData = async () => {
-        console.log("entered")
         try {
           const data = await backend_URL.get('/user/get-data')
-          console.log(data)
           if (data.data.message === 'Data Incoming!') {
               const datasData = data?.data?.data
               setBrosGenres(datasData?.brosGenres)
