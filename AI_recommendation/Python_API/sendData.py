6b21d0e1432e36ebcc022e7fbbd6301a5a5ae751
Piyush-bansaI <kajal891bansal@gmail.com>
2026-02-01 20:29:30 +0530

    did some optimization

--- AI_recommendation/Python_API/sendData.py
+++ AI_recommendation/Python_API/sendData.py
@@ -7,47 +7,61 @@ load_dotenv()
 from AnimeRecommender import animeRecommender
 from MangaRecommender import mangaRecommender
 
-from MangaRecommender import mangaRecommender
 
 app = Flask(__name__)
 CORS(app)
 
+@app.errorhandler(Exception)
+def AIError(e):
+    print('\033[91merror: \033[0m', e)
+    return jsonify({
+        "message": "their is an error in AI recommender"
+    }), 500
+
 @app.get('/')
 def Running():
     return {
-        "status": 200,
         "message": "Server Running"
-    }
+    }, 200
 
 @app.post('/recommend-anime')
 def recommendAnime():
-    ANIbro_data = request.get_json()
+    ANIbro_data = request.get_json(silent=True)
+
+    if not ANIbro_data:
+        return jsonify({
+            'message': 'didn\'t got the genre'
+        }), 400
+
     Anime_genres = ANIbro_data.get('genres', [])
 
     if len(Anime_genres) == 0: 
         return jsonify({
-                "status": 400,
                 "message": "Didn't got the genres"
-                })
+                }), 400
         
     recommended_genre = animeRecommender(Anime_genres)
 
     return jsonify({
-        "status": 200,
         "message": "Genre Incoming!",
         "genre": recommended_genre
-    })
+    }), 200
     
 @app.post('/recommend-manga')
 def recommendManga():
-    ANIbro_data = request.get_json()
+    ANIbro_data = request.get_json(silent=True)
+
+    if not ANIbro_data:
+        return jsonify({
+            'message': 'didn\'t got the genre'
+        }), 400
+
     Manga_genres = ANIbro_data.get('genres', [])
 
     if len(Manga_genres) == 0:
         return jsonify({
-            "status": 400,
             "message": "Didn't got the genres"
-        })
+        }), 400
 
     recommended_genre = mangaRecommender(Manga_genres)
 
@@ -55,7 +69,7 @@ def recommendManga():
         "status": 200,
         "message": "Genre Incoming!",
         "genre": recommended_genre
-    })
+    }), 200
 
 if __name__ == "__main__":
     debug = os.environ.get("NODE_ENV") != 'production'
