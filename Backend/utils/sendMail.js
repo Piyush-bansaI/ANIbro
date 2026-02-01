1192f567c3283bc422772d18e17724d47139b82e
Piyush-bansaI <kajal891bansal@gmail.com>
2026-02-01 13:35:33 +0530

    Update sendMail.js

--- Backend/utils/sendMail.js
+++ Backend/utils/sendMail.js
@@ -23,7 +23,7 @@ const sendEmail = async (token, username, email) => {
                     },
                     to: [{
                         email: email,
-                        username: username
+                        name: username
                     }],
                     subject: `ANIbro verification Link`,
                     htmlContent: finalBody
@@ -34,8 +34,7 @@ const sendEmail = async (token, username, email) => {
                         'Content-Type': 'application/json'
                     }
                 }
-            );
-            console.log("\x1b[92msent mail using Brevo\x1b[0m")
+            ).finally(res => console.log("\x1b[92msent mail using Brevo\x1b[0m"))
         } else {
             console.log("x1b[95msending mail using smtpx1b[0m")
             await defineMail.sendMail({
