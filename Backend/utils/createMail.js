5efdea17b5c8267c3603c881ccd62abd1b6923dd
Piyush-bansaI <kajal891bansal@gmail.com>
2026-02-01 12:37:15 +0530

    Update createMail.js

--- Backend/utils/createMail.js
+++ Backend/utils/createMail.js
@@ -3,16 +3,16 @@ const nodemailer = require('nodemailer');
 let transporter;
 
 if (process.env.NODE_ENV !== "production") {
-  console.log("\x1b[94myou are in development mode!\x1b[0m")
-  transporter = nodemailer.createTransport({
-    service: "gmail",
-    secure: true,
-    port: 465,                         
-    auth: {
-      user: process.env.ANIbro_mail,      
-      pass: process.env.ANIbro_pass,      
-    },
-  });
+const defineMail = () => {
+    transporter = nodemailer.createTransport({
+      service: "gmail",
+      secure: true,
+      port: 465,                         
+      auth: {
+        user: process.env.ANIbro_mail,      
+        pass: process.env.ANIbro_pass,      
+      },
+    });
+  }
+  module.exports = defineMail
 }
-
-module.exports = defineMail
